e77c90a863454ae1b50fc9a67a50c7b44f6e38b2
Alexandre HERRERO <alexandre.herrero@live.fr>
2016-11-14 16:59:32 +0100

    first commit

---
+++ header.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Intégration</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+    <!-- jQuery -->
+    <script src="bootstrap/js/jquery-3.1.0.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <script type="text/javascript" src="js/ytresponsive.js"></script>
+    <script type="text/javascript" src="js/percircle.js"></script>
+    <script type="text/javascript">
+        $(function(){
+            $("[id$='circle']").percircle();
+        });
+    </script>
+    <link rel="stylesheet" href="css/percircle.css">
+</head>
+
+<body>
+
+<!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#"><img class="img-responsive" src="img/logo.png" alt=""></a>
+            </div>
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a class="#conhead" href="#">Accueil</a>
+                    </li>
+                    <li>
+                        <a href="#section2">Présentation</a>
+                    </li>
+                    <li>
+                        <a href="#section3">Spécialités</a>
+                    </li>
+                    <li>
+                        <a href="#section4">Honoraires</a>
+                    </li>
+                    <li>
+                        <a href="#footer">Contact</a>
+                    </li>
+                    <li>
+                        <a href="#">Blog</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
---
+++ index.php
@@ -0,0 +1,230 @@
+<?php require 'header.php' ?>
+
+    <!---- Header sous menu ----->
+
+    <div id="conhead" class="container-fluid conhead">
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <a class="link link--ilin logo" href="#"><span>A</span><span>H</span></a>
+                <p class="lead section-lead logo"><span>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</span></p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12 text-center logo">
+                <button class="btn btn-head" type="submit">Button</button>
+                <button class="btn btn-head" type="submit">Button</button>
+            </div>
+        </div>
+    </div>
+</header>
+
+<section id="section2" class="section2 section-margin">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <h1 class="section-heading">WEBSITE</h1>
+                <hr class="blackhr">
+                <p class="lead section-lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </div>
+        </div>
+        <div class="row fade4col">
+            <div class="col-md-3 text-center fade-left">
+                <img class="img-responsive imgcenter" src="img/branding.png" alt="">
+                <h3>BRANDING</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </div>
+            <div class="col-md-3 text-center fade-left">
+                <img class="img-responsive imgcenter" src="img/design.png" alt="">
+                <h3>DESIGN</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </div>
+            <div class="col-md-3 text-center fade-right">
+                <img class="img-responsive imgcenter" src="img/flat.png" alt="">
+                <h3>FLAT</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </div>
+            <div class="col-md-3 text-center fade-right">
+                <img class="img-responsive imgcenter" src="img/responsive.png" alt="">
+                <h3>RESPONSIVE</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- Content Section -->
+<section id="section3" class="section3 section-margin hideme">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <h1 class="section-heading">MONEY</h1>
+                <hr class="blackhr">
+                <p class="lead section-lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-3 text-center">
+                <i class="fa fa-eur" aria-hidden="true"></i>
+                <h3>EURO</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </div>
+            <div class="col-md-3 text-center">
+                <i class="fa fa-usd" aria-hidden="true"></i>
+                <h3>DOLLAR</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </div>
+            <div class="col-md-3 text-center">
+                <i class="fa fa-btc" aria-hidden="true"></i>
+                <h3>BITCOIN</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </div>
+            <div class="col-md-3 text-center">
+                <i class="fa fa-gbp" aria-hidden="true"></i>
+                <h3>GBP</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </div>
+        </div>
+
+    </div>
+</section>
+
+
+
+<!-- Content Section -->
+<div class="parallax1">
+    <div class="conparallax">
+<section id="section4" class="section4 section-margin hideme">
+    <div class="container">
+
+        <div class="row text-center">
+            <div class="col-md-12">
+                <h1 class="section-heading">SKILLS</h1>
+                <hr class="whitehr">
+                <p class="lead section-lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-3 col-sm-12 col-xs-12 text-center">
+                <div id="bluecircle" data-percent="20" class="big"></div>
+                <h3>HTML/CSS</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </div>
+
+            <div class="col-md-3 col-sm-12 col-xs-12 text-center">
+                <div id="bluecircle" data-percent="40" class="big"></div>
+                <h3>JAVASCRIPT</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </div>
+
+            <div class="col-md-3 col-sm-12 col-xs-12 text-center">
+                <div id="bluecircle" data-percent="60" class="big"></div>
+                <h3>PHP/MYSQL</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </div>
+
+            <div class="col-md-3 col-sm-12 col-xs-12 text-center">
+                <div id="bluecircle" data-percent="80" class="big imgcenter"></div>
+                <h3>PHOTOSHOP</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </div>
+        </div>
+    </div>
+        </div>
+    </div>
+</section>
+
+
+<!-- Footer -->
+<footer id="footer">
+    <div class="container conmail">
+        <div class="row">
+        </div>
+        <div class="row">
+        <div class="col-md-6">
+            <h1 class="section-heading">HORAIRES ET CONTACT</h1>
+            <hr class="whitehr">
+            <p class="section-paragraph">Pole Léonard de Vinci - 12 Avenue Léonard de Vinci, 92400 Courbevoie</p>
+            <div class='embed-container'><iframe src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2623.0254584363715!2d2.23422031567572!3d48.895851979291095!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e665002d2c6665%3A0x9595552cb384bd7!2sP%C3%B4le+Universitaire+L%C3%A9onard+de+Vinci!5e0!3m2!1sfr!2sfr!4v1475502359120' width='800' height='400' frameborder='0' style='border:0' allowfullscreen></iframe></div>
+        </div>
+            <hr class="visible-xs hrmobile">
+        <div class="col-md-6">
+            <input type="text" class="form-control" placeholder="Nom et prénom">
+            <input type="email" class="form-control" placeholder="Email">
+            <input type="number" class="form-control" placeholder="Numéro">
+            <textarea class="form-control" rows="7" placeholder="Votre message"></textarea>
+            <br>
+            <div class="text-center">
+            <button class="btn btn-head" type="submit">ENVOI</button>
+            </div>
+        </div>
+        </div>
+            <br>
+        <div class="row">
+            <div class="col-md-12 text-center">
+
+            </div>
+        </div>
+        <div class="row social">
+            <div class="col-md-2 col-sm-2 col-xs-4 text-center">
+                <i class="fa fa-facebook" aria-hidden="true"></i>
+            </div>
+            <div class="col-md-2 col-sm-2 col-xs-4 text-center">
+                <i class="fa fa-twitter" aria-hidden="true"></i>
+            </div>
+            <div class="col-md-2 col-sm-2 col-xs-4 text-center">
+                <i class="fa fa-instagram" aria-hidden="true"></i>
+            </div>
+            <div class="col-md-2 col-sm-2 col-xs-4 text-center">
+                <i class="fa fa-linkedin" aria-hidden="true"></i>
+            </div>
+            <div class="col-md-2 col-sm-2 col-xs-4 text-center">
+                <i class="fa fa-dribbble" aria-hidden="true"></i>
+            </div>
+            <div class="col-md-2 col-sm-2 col-xs-4 text-center">
+                <i class="fa fa-git" aria-hidden="true"></i>
+            </div>
+        </div>
+    </div>
+</footer>
+
+
+<script>$(function(){  // $(document).ready shorthand
+    $('.monster').fadeIn('slow');
+});
+
+$(document).ready(function() {
+
+    /* Every time the window is scrolled ... */
+    $(window).scroll( function(){
+
+        /* Check the location of each desired element */
+        $('.hideme').each( function(i){
+
+            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+
+                $(this).animate({'opacity':'1'},1000);
+
+            }
+
+        });
+
+    });
+
+});</script>
+
+<script type="text/javascript">
+    $(function(){
+        $("[id$='circle']").percircle();
+    });
+</script>
+
+
+
+
+</body>
+
+</html>
