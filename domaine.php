c0c7decc9ad9b698e3af8fb07b7689c472abbfbc
Charo <diridolloumarwan@gmail.com>
2016-11-29 19:08:58 +0100

    domaine

---
+++ domaine.php
@@ -0,0 +1,64 @@
+<?php require 'header.php' ?>
+
+
+<section>
+    <div class="container">
+        <div class="row">
+             <div class="col-lg-12 text-center">
+                <h2 class="section-heading">NOURRISSON</h2>
+             </div>
+        </div>
+        <hr class="blackhr">
+        <div class="row">
+             <div class="col-md-7">
+                <p style="text-align: justify;">La gestation, période pendant laquelle le bébé est dans le ventre de sa maman, peut amener ce dernier à subir des contraintes. En effet une posture in-utéro maintenue au long terme peut modifier la tonicité musculaire et la mobilité articulaires. Aussi les éventuels différents traumatismes survenant de l’extérieur peuvent avoir une répercussion sur le bébé et la maman.
+                L’accouchement est considéré comme le premier traumatisme de la vie. Avec un travail trop long, trop court, l’utilisation d’instruments (forceps, cuillères, ventouses) ou de techniques obstétrique (traction de la tête ou d’un membre) il peut perturber le fonctionnement normal des structures crâniennes et alors se répercuter sur son développement lors de la croissance. 
+                C’est pourquoi l’ostéopathe contrôle et rétablis l’équilibre mécanique du nouveau-né afin d’en optimiser son éveil. </p>
+             </div>
+             <div class="col-md-5 text-center">
+             	<img src="img/nourrisson.jpg" class="img-responsive center-block" alt="image de nourrisson">
+        	</div>
+    	</div>
+    </div>    
+</section>
+
+    
+
+
+
+<section>
+    <div class="container">
+    	<div class="row">
+             <div class="col-lg-12 text-center">
+                <h2 class="section-heading">QUAND CONSULTER SON OSTÉOPATHE ?</h2>
+             </div>
+        </div>
+        <hr class="blackhr">
+
+    	<div class="row">
+             <div class="col-lg-5 text-center">
+             	<img src="img/nourrisson2.jpg" class="img-responsive center-block" alt="image de nourrisson">
+             </div>
+             <div class="col-md-7">
+             	<ul>
+		            <li>Pour un bilan après la naissance</li>
+		            <li>Bilan préventif annuel</li>
+		            <li>Accouchement long (+ de 8h) ou court (- de 2h)</li>
+		            <li>Accouchement sous péridurale, par césarienne, par instrumentalisation (forceps, ventouse, spatule)</li>
+		            <li>Manœuvre de traction ou de pression sur l’abdomen (souvent lieu lors de césarienne)</li>
+		            <li>Présentation en siège ou par la face </li>
+		            <li>Grosses gémellaire (augmentation des pressions)</li>
+		            <li>Naissance prématurée / souffrance fœtale / réanimation</li>
+		            <li>Asymétrie crânienne / plagiocéphalie (déformation du crâne)</li>
+		            <li>Torticolis congénital / Rotation de la tête préférentielle</li>
+		            <li>Troubles digestifs : reflux, régurgitations, diarrhées (coliques), constipations</li>
+		            <li>Troubles du sommeil / agitation</li>
+		            <li>Cris ou pleurs incessants</li>
+		            <li>Hyper / Hypo-activité</li>
+		            <li>Retard de l’apprentissage ou du développement psychomoteur</li>
+	        	</ul>
+             </div>
+        </div>  
+    </div>
+</section>
+
